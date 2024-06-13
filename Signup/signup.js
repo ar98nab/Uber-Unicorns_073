722af841c8bbdd3d0637a2ff750abcbef7423e36
Om Shrestha <omshre06@gmail.com>
2024-06-14 00:40:08 +0530

    compleate Signup page UI

---
+++ Signup/signup.js
@@ -0,0 +1,24 @@
+
+const slides=document.querySelectorAll(".slide")
+let slideindex=0
+let intervelid=null;
+
+
+initialslider()
+
+function initialslider(){
+
+    slides[slideindex].classList.add("displaySlides")
+}
+
+function slideshow(index){
+
+}
+
+function prevSlide(){
+
+}
+
+function nextSlide(){
+
+}
